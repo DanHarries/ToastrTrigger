fda0bd9ee8a3040cffe8f8adc90518d4c4eb330f
itharriesd <itharriesd@ICS3314T>
2019-02-22 11:55:48 +0000

    features implemented

---
+++ Helpers/StatusMessageDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToastrTest.Helpers
+{
+    public class StatusMessageDisplay
+    {
+        public string Message { get; set; }
+    }
+
+    public static class DisplayMessageHelper
+    {
+        public static StatusMessageDisplay DisplayTempData(string Message)
+        {
+            StatusMessageDisplay _StatusMessage = new StatusMessageDisplay()
+            {
+                Message = Message
+            };
+            return _StatusMessage;
+        }
+    }
+
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ToastrTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
